58b4165317a581f17a04e6a013ed1567790b1fa5
Bedirhan Yıldırım <bedirhan.yildirim@stu.fsm.edu.tr>
2020-05-18 01:38:22 +0300

    project created

---
+++ P2P-Messaging-App/src/client/Client.java
@@ -0,0 +1,18 @@
+package client;
+
+/**
+ *
+ * @author Bedirhan Yıldırım | bedirhan.yildirim@stu.fsm.edu.tr
+ * @description 
+ * @file Client.java
+ * @assignment Computer System Security Programming Assignment
+ * @date 18.05.2020
+ * 
+ */
+public class Client {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
---
+++ P2P-Messaging-App/src/server/Server.java
@@ -0,0 +1,18 @@
+package server;
+
+/**
+ *
+ * @author Bedirhan Yıldırım | bedirhan.yildirim@stu.fsm.edu.tr
+ * @description 
+ * @file Server.java
+ * @assignment Computer System Security Programming Assignment
+ * @date 18.05.2020
+ * 
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
